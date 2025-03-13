a840c1dc2d2e640f2172513ab3ad88de46fba818
BabulHoda98 <babulhoda98@gmail.com>
2025-03-14 00:09:23 +0530

    props-drilling pass data

--- src/topics/06-context-api/prop-drilling/usercontainer.jsx
+++ src/topics/06-context-api/prop-drilling/usercontainer.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 const UserContainer = ({user,logout}) => {
   return (
     <div className='flex gap-2 items-center'>
-        <p >Hello There,{user.name}</p>
-        <button className='btn'onClick={logout}>Logout</button>
+        {uesr ? (
+            <>
+                <p >Hello There,{user.name}</p>
+                <button className='btn'onClick={logout}>Logout</button>
+            </>
+        ):(
+            <p>Please login</p>
+        )}
+        
     </div>
   )
 }
